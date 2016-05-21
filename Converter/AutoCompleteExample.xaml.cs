c2c517ab076d60fe76b60f8cee52d16ce2ce6ae6
meermalik93 <meermalik93@gmail.com>
2016-05-21 15:49:44 +0530

    changes

--- Converter/AutoCompleteExample.xaml.cs
+++ Converter/AutoCompleteExample.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Collections.ObjectModel;
 
-namespace converter
+namespace Example
 {
     /// <summary>
     /// Interaction logic for AutoCompleteExample.xaml
